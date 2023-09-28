36bcebe401e87e479aeb481e21a0839996db023e
gavhart <har19076@byui.edu>
2023-09-28 11:23:02 -0600

    team project

--- src/js/product.js
+++ src/js/product.js
@@ -1,5 +1,12 @@
 import { setLocalStorage } from "./utils.mjs";
 import { findProductById } from "./productData.mjs";
+import { getParam } from  "./utils.mjs";
+import productDetails from "./productDetails.mjs";
+
+const productId = getParam("product");
+productDetails(productId, ".productDetails")
+
+console.log(await findProductById(productId));
 
 function addProductToCart(product) {
   setLocalStorage("so-cart", product);
