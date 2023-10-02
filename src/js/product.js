7b7a7e6ce7b014567ddb954c6871f501e39dab0b
cal20014 <cal20014@byui.edu>
2023-10-02 14:37:34 -0600

    Added template code to productDetailsTemplate(). Built out productDetails()

--- src/js/product.js
+++ src/js/product.js
@@ -1,29 +1,10 @@
-import { setLocalStorage } from "./utils.mjs";
-import { getLocalStorage } from "./utils.mjs";
 import { findProductById } from "./productData.mjs";
-import { getParam } from  "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import productDetails from "./productDetails.mjs";
+import { addProductToCart } from "./productDetails.mjs";
 
 const productId = getParam("product");
-productDetails(productId, ".productDetails")
-
-console.log(await findProductById(productId));
-
-function addProductToCart(product) {
-  // Get the existing cart data from local storage
-  let cart = getLocalStorage("so-cart");
-
-  // If the cart data doesn't exist, initialize an empty array for the cart
-  if (!cart) {
-    cart = [];
-  }
-
-  // Add the new product to the cart
-  cart.push(product);
-
-  // Store the updated cart data back in local storage
-  setLocalStorage("so-cart", cart);
-}
+productDetails(productId, ".product-detail");
 
 // add to cart button event handler
 async function addToCartHandler(e) {
--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -1,10 +1,55 @@
-export default productDetails(productId, selector)
+import { findProductById } from "./productData.mjs";
+import { getLocalStorage } from "./utils.mjs";
+import { setLocalStorage } from "./utils.mjs";
 
-function addProductToCart(product) {
-    setLocalStorage("so-cart", product);
+let product = {};
+
+export default async function productDetails(productId, selector) {
+  // use findProductById to get the details for the current product. findProductById will return a promise! use await or .then() to process it
+  product = await findProductById(productId);
+  // once we have the product details we can render out the HTML
+  const element = document.querySelector(selector);
+  element.insertAdjacentHTML("afterBegin", productDetailsTemplate(product));
+  // add a listener to Add to Cart button
+  document
+    .querySelector("#addToCart")
+    .addEventListener("click", addProductToCart);
+}
+
+function addProductToCart() {
+  // Get the existing cart data from local storage
+  let cart = getLocalStorage("so-cart");
+
+  // If the cart data doesn't exist, initialize an empty array for the cart
+  if (!cart) {
+    cart = [];
   }
 
-function productDetailsTemplate(product) 
+  // Add the new product to the cart
+  cart.push(product);
 
-let productData = {}
+  // Store the updated cart data back in local storage
+  setLocalStorage("so-cart", cart);
+}
 
+function productDetailsTemplate(item) {
+  return `<h3>${item.Brand.Name}</h3>
+
+  <h2 class="divider">${item.NameWithoutBrand}</h2>
+
+  <img
+    class="divider"
+    src="${item.Image}"
+    alt="${item.Name}"
+  />
+
+  <p class="product-card__price">$${item.FinalPrice}</p>
+
+  <p class="product__color">${item.Color[0].ColorName}</p>
+
+  <p class="product__description">${item.DescriptionHtmlSimple}</p>
+
+  <div class="product-detail__add">
+    <button id="addToCart" data-id="${item.Id}">Add to Cart</button>
+  </div>`;
+}
