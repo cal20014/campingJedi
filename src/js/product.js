e965739db78efbde791f1fab6319961c58e76749
cal20014 <cal20014@byui.edu>
2023-10-02 12:59:48 -0600

    Changed addProductToCart function so that it appends an array or objects. to save into local storage

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,23 @@
 import { setLocalStorage } from "./utils.mjs";
+import { getLocalStorage } from "./utils.mjs";
 import { findProductById } from "./productData.mjs";
 
 function addProductToCart(product) {
-  // check if product exists if so append it on the end of the array
-  // makeproducts instead of product
-  setLocalStorage("so-cart", product);
+  // Get the existing cart data from local storage
+  let cart = getLocalStorage("so-cart");
+
+  // If the cart data doesn't exist, initialize an empty array
+  if (!cart) {
+    cart = [];
+  }
+
+  // Add the new product to the cart
+  cart.push(product);
+
+  // Store the updated cart data back in local storage
+  setLocalStorage("so-cart", cart);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await findProductById(e.target.dataset.id);
