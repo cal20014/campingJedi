c710ce794a212157a96270025da752b1d0289dac
MagnesiumSail <Leapster16@gmail.com>
2023-10-05 11:10:40 -0600

    Finished Team 3 Task

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,14 @@
 // import ExternalServices from './externalServices.js';
 // import ProductList from './productList.js';
-import { loadHeaderFooter } from './utils.js';
+//import { loadHeaderFooter } from './utils.mjs';
+import ProductList from "./components/ProductList.svelte"
 
-loadHeaderFooter();
+new ProductList({
+    target: document.querySelector(".products"),
+    props: { category: "tents" },
+})
+
+//loadHeaderFooter();
 
 // const dataSource = new ExternalServices('tents');
 // let element = document.querySelector('.product-list');
