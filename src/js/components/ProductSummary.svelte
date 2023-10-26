1d055e6ea4c872aab7d8f6e4a43aca631e00b90e
david <davraya4@gmail.com>
2023-10-25 18:30:52 -0600

    added quick look up to display description in product list

--- src/js/components/ProductSummary.svelte
+++ src/js/components/ProductSummary.svelte
@@ -1,13 +1,47 @@
 <script>
     export let productCard;
+
+    console.log(productCard.DescriptionHtmlSimple);
+
+  let showModal = false; // A variable to control the visibility of the modal
+
+  // Function to show the modal
+  function show() {
+    showModal = true;
+  }
+
+  // Function to hide the modal
+  function hide() {
+    showModal = false;
+  }
+
 </script>
 
 
 <a href="../product_pages/index.html?product={productCard.Id}">
-    <img
-      src={ productCard.Images.PrimaryMedium }
-      alt="{ productCard.Brand.Name } || { productCard.Name }"
-    />
-    <h3 class="card__brand">{ productCard.Brand.Name }</h3>
-    <h2 class="card__name">{ productCard.Name}</h2>
-    <p class="product-card__price">{ productCard.ListPrice}</p></a>
+  <img
+    src={ productCard.Images.PrimaryMedium }
+    alt="{ productCard.Brand.Name } || { productCard.Name }"
+  />
+  <h3 class="card__brand">{ productCard.Brand.Name }</h3>
+  <h2 class="card__name">{ productCard.Name}</h2>
+  <p class="product-card__price">{ productCard.ListPrice}</p></a>
+  
+{#if !showModal}
+  <button class='showBtn' on:click={show}>Show Description</button>
+{/if}
+
+{#if showModal}
+  <div class="modal">
+    {@html productCard.DescriptionHtmlSimple}
+    <button class='showBtn' on:click={hide}>Close Description</button>
+  </div>
+{/if}
+
+<style>
+
+.showBtn{
+  font-size: 0.8em;
+}
+
+</style>
